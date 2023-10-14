b07ae1da84987e6850a2fc00e4786f67821d2c68
Marcus Hultman <marcus.hultman1@gmail.com>
2023-10-14 11:28:30 -0400

    add middleware with CSP

---
+++ routes/_middleware.ts
@@ -0,0 +1,9 @@
+import { MiddlewareHandlerContext } from "$fresh/server.ts";
+
+const baseUrl = Deno.env.get("BASE_URL") ?? "http://localhost:8080";
+
+export async function handler(_: Request, ctx: MiddlewareHandlerContext) {
+  const res = await ctx.next();
+  res.headers.set("Content-Security-Policy", `default-src 'self' ${baseUrl} `);
+  return res;
+}
