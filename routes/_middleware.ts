b6c9b8f8cd9797796b3e39068fd634e9131596c6
Marcus Hultman <marcus.hultman1@gmail.com>
2023-10-14 11:33:28 -0400

    +'unsafe-inline'

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -4,6 +4,9 @@ const baseUrl = Deno.env.get("BASE_URL") ?? "http://localhost:8080";
 
 export async function handler(_: Request, ctx: MiddlewareHandlerContext) {
   const res = await ctx.next();
-  res.headers.set("Content-Security-Policy", `default-src 'self' ${baseUrl} `);
+  res.headers.set(
+    "Content-Security-Policy",
+    `default-src 'self' 'unsafe-inline' ${baseUrl} `,
+  );
   return res;
 }
