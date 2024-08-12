d62aab0319bad7d84ea4840f53d3052ccba90850
Marcus Hultman <marcus.hultman1@gmail.com>
2024-08-11 21:10:37 -0400

    /settings2: UI update

--- routes/settings2.ts
+++ routes/settings2.ts
@@ -1,7 +1,7 @@
 import { Handlers } from "$fresh/server.ts";
 import { encode } from "$std/encoding/base64.ts";
 import { createCanvas, encodeCanvas } from "../src/rendering.ts";
-import { decodeServiceRequest } from "../src/state.ts";
+import { decodeServiceRequest, Prio } from "../src/state.ts";
 import { encodeState } from "../src/state.ts";
 import { makeResponse } from "../src/state.ts";
 
@@ -14,6 +14,27 @@ const parseNumber = (n: string | null) => n ? Number(n) : undefined;
 
 const CANVAS = createCanvas(23, 16);
 
+// deno-fmt-ignore
+const LENGTHS = [
+  [ 1, 1 ], [ 4, 4 ], [ 2, 2 ],
+  [ 2, 2 ], [ 1, 1 ], [ 2, 2 ],
+  [ 8, 8 ], [ 6, 6 ], [ 6, 6 ],
+  [ 4, 4 ], [ 8, 8 ], [ 8, 8 ],
+  [ 3, 3 ], [ 3, 3 ], [ 8, 8 ],
+  [ 8, 8 ], [ 7, 7 ], [ 4, 4 ],
+  [ 5, 5 ], [ 2, 2 ], [ 7, 7 ],
+  [ 6, 6 ], [ 1, 1 ]
+];
+
+function makeDisplay(brightness = 0) {
+  const ctx = CANVAS.getContext("2d");
+  ctx.clearRect(0, 0, 23, 16);
+  ctx.fillStyle = "white";
+  const end = 23 * (brightness ?? 0) / 64;
+  LENGTHS.slice(0, end).forEach(([l0, l1], x) => ctx.fillRect(x, 8 - l0, 1, l0 + l1));
+  return encodeCanvas(CANVAS);
+}
+
 export const handler: Handlers = {
   async POST(req) {
     let { data: { brightness, hue } } = await decodeServiceRequest<Data>(req, {});
@@ -22,19 +43,17 @@ export const handler: Handlers = {
       parseNumber(searchParams.get("brightness")) ?? brightness,
       parseNumber(searchParams.get("hue")) ?? hue,
     ];
-
-    const ctx = CANVAS.getContext("2d");
-    ctx.resetTransform();
-    ctx.clearRect(0, 0, 23, 16);
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 6, 23 * (brightness ?? 0) / 64, 4);
-
     return makeResponse({
       "/settings2": encodeState({ brightness, hue }),
-      "/timeout/brightness": encodeState({ date: new Date() }, {
-        logo: encode(new Uint8Array([0xff, 0xff, 0xff])),
-        bytes: encodeCanvas(CANVAS),
-      }, { poll: 3000 }),
+      ...searchParams.size
+        ? {
+          "/timeout/brightness": encodeState({ date: new Date() }, {
+            logo: encode(new Uint8Array([0xff, 0xff, 0xff])),
+            bytes: makeDisplay(brightness),
+            prio: Prio.NOTIFICATION,
+          }, { poll: 3000 }),
+        }
+        : {},
     });
   },
 };
