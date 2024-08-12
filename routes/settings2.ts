933e8c0269e089c14d63d0f15f3327c7bc529bbc
Marcus Hultman <marcus.hultman1@gmail.com>
2024-08-11 20:50:29 -0400

    /settings2: brightness bar UI

--- routes/settings2.ts
+++ routes/settings2.ts
@@ -1,4 +1,6 @@
 import { Handlers } from "$fresh/server.ts";
+import { encode } from "$std/encoding/base64.ts";
+import { createCanvas, encodeCanvas } from "../src/rendering.ts";
 import { decodeServiceRequest } from "../src/state.ts";
 import { encodeState } from "../src/state.ts";
 import { makeResponse } from "../src/state.ts";
@@ -10,6 +12,8 @@ interface Data {
 
 const parseNumber = (n: string | null) => n ? Number(n) : undefined;
 
+const CANVAS = createCanvas(23, 16);
+
 export const handler: Handlers = {
   async POST(req) {
     let { data: { brightness, hue } } = await decodeServiceRequest<Data>(req, {});
@@ -18,6 +22,19 @@ export const handler: Handlers = {
       parseNumber(searchParams.get("brightness")) ?? brightness,
       parseNumber(searchParams.get("hue")) ?? hue,
     ];
-    return makeResponse({ "/settings2": encodeState({ brightness, hue }) });
+
+    const ctx = CANVAS.getContext("2d");
+    ctx.resetTransform();
+    ctx.clearRect(0, 0, 23, 16);
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 6, 23 * (brightness ?? 0) / 64, 4);
+
+    return makeResponse({
+      "/settings2": encodeState({ brightness, hue }),
+      "/timeout/brightness": encodeState({ date: new Date() }, {
+        logo: encode(new Uint8Array([0xff, 0xff, 0xff])),
+        bytes: encodeCanvas(CANVAS),
+      }, { poll: 3000 }),
+    });
   },
 };
