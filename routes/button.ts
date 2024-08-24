254b938522adbb9caab3d86afdd07b8ab382d698
Marcus Hultman <marcus.hultman1@gmail.com>
2024-08-24 11:45:38 -0400

    /button: render someting

--- routes/button.ts
+++ routes/button.ts
@@ -1,8 +1,19 @@
 import { Handlers } from "$fresh/server.ts";
-import { makeResponse } from "../src/state.ts";
+import { encode } from "$std/encoding/base64.ts";
+import { makeDisplay } from "../src/rendering.ts";
+import { encodeState, makeResponse } from "../src/state.ts";
 
 export const handler: Handlers = {
-  async POST(req) {
-    return makeResponse({});
+  POST() {
+    return makeResponse({
+      "/button": encodeState(undefined, {
+        logo: encode(new Uint8Array([0xFF, 0xFF, 0xFF])),
+        bytes: makeDisplay((ctx) => {
+          ctx.fillStyle = "white";
+          ctx.fillRect(0, 0, 10, 10);
+        }),
+      }),
+      "/timeout/button": encodeState(undefined, undefined, { poll: 3000 }),
+    });
   },
 };
