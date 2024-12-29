3ae4908f32bd37e0447c232334844a92bb5a330b
Marcus Hultman <marcus.hultman1@gmail.com>
2024-12-28 19:48:47 -0500

    /led/ticker: refactor

--- routes/led/ticker.ts
+++ routes/led/ticker.ts
@@ -1,9 +1,18 @@
 import { Handlers } from "$fresh/server.ts";
 import { encode } from "$std/encoding/base64.ts";
-import { createCanvas, encodeCanvas } from "../../src/rendering.ts";
+import { parseData } from "../../src/state.ts";
 import { makeResponse, makeState, Prio } from "../../src/state.ts";
+import { timeOfDayBrightness } from "../../src/time_of_day_brightness.ts";
+import { createCanvas, encodeCanvas } from "../../src/rendering.ts";
+
+enum Stage {
+  VALUE = 1,
+  DAY,
+}
 
 const API_KEY = Deno.env.get("TICKER_API_KEY");
+const makeAPIUrl = (symbol: string) =>
+  `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${API_KEY}`;
 
 async function toEntry(res: Response) {
   const data = await res.json();
@@ -15,64 +24,65 @@ async function toEntry(res: Response) {
   };
 }
 
-let canvas = createCanvas(23, 16);
+type Data = Record<string | number | symbol, never> | {
+  stage: Stage;
+  entry: Awaited<ReturnType<typeof toEntry>>;
+};
 
-const CHAR_WIDTH = (() => {
-  const ctx = canvas.getContext("2d");
-  ctx.scale(0.8, 1);
-  ctx.font = "14px monospace";
-  return ctx.measureText(" ").width;
-})();
+const canvas = createCanvas(23, 16);
+let isFontRegistered = false;
 
 export const handler: Handlers = {
   POST: async (req) => {
-    const url = new URL(req.url);
-    const symbols = [...url.searchParams.keys()];
+    const data = await parseData<Data>(req, {});
+    const { stage, entry } = "stage" in data ? (++data.stage, data) : await (async () => {
+      const url = new URL(req.url);
+      const symbols = [...url.searchParams.keys()];
 
-    if (!symbols.length) {
+      if (!symbols.length) {
+        return {};
+      }
+      const [entry] = await Promise.all(
+        symbols.map((symbol) => fetch(makeAPIUrl(symbol)).then(toEntry)),
+      );
+      return { stage: Stage.VALUE, entry };
+    })();
+
+    if (!isFontRegistered) {
+      const res = await fetch(new URL("/static/enter-the-gungeon-big.ttf", req.url));
+      canvas.loadFont(await res.bytes(), { family: "7px" });
+      isFontRegistered = true;
+    }
+
+    if (!stage || stage > Stage.DAY) {
       return makeResponse({ "/led/ticker": null });
     }
 
-    const [entry] = await Promise.all(
-      symbols.map((symbol) =>
-        fetch(
-          `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${API_KEY}`,
-        ).then(toEntry)
-      ),
-    );
-
     const kv = await Deno.openKv();
-    const { value } = await kv.get<{ brightness: number }>(["settings"]);
-    const { brightness = 31 } = value ?? {};
+    const { value } = await kv.get<{ brightness: number; hue: number }>(["settings"]);
+    const { brightness = 31, hue = 255 } = value ?? {};
 
-    const text = `${entry.symbol} ${entry.value} ${entry.day}`;
-    const width = CHAR_WIDTH * (text.length + 1);
-    const xscroll = 10;
-    const timeout = width * 1000 / xscroll;
-
-    if (width > canvas.width) {
-      canvas = createCanvas(width, 16);
-    }
+    const color = timeOfDayBrightness({ brightness, hue });
+    const [r, g, b] = color;
 
     const ctx = canvas.getContext("2d");
-    ctx.resetTransform();
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.scale(0.8, 1);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = entry.day >= 0 ? `rgb(0,${brightness},0)` : `rgb(${brightness},0,0)`;
-    ctx.font = "14px monospace";
-    ctx.fillText(text, 0, 13);
+    ctx.font = "9px 7px";
+    ctx.fillStyle = `rgb(${r},${g},${b})`;
+    ctx.fillText(entry.symbol, 0, 7);
+    ctx.fillText(stage === Stage.VALUE ? `$${entry.value}` : `$${entry.day}`, 0, 15);
 
     return makeResponse({
-      "/led/ticker": makeState({
+      "/led/ticker": makeState<Data>({
+        data: { stage, entry },
         display: {
-          logo: encode(new Uint8Array([0xFF, 0xFF, 0xFF])),
-          bytes: encodeCanvas(canvas, width),
-          width,
-          xscroll,
+          logo: encode(new Uint8Array(color)),
+          bytes: encodeCanvas(canvas),
           prio: Prio.NOTIFICATION,
         },
-        timeout,
+        poll: 2500,
       }),
     });
   },
